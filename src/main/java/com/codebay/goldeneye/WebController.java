a8807c730c3d76d1cffccb3424bbc547a2fac9ce
AiramLD <airamluisdiaz629@gmail.com>
2024-03-18 14:56:33 +0000

    queda poco

--- src/main/java/com/codebay/goldeneye/WebController.java
+++ src/main/java/com/codebay/goldeneye/WebController.java
@@ -1,15 +1,30 @@
 package com.codebay.goldeneye;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;  
 import org.springframework.ui.Model;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class WebController {  
-    @GetMapping("/")
-    public String index() {
-        return "index";
+   @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(
+        @RequestParam("name") String name,
+        @RequestParam("surname") String surname,
+        @RequestParam("office") String office,
+        @RequestParam("department") String department,
+        Model model
+    ) {
+
+        char nameMail = name.charAt(0);
+        String surnameMail = surname.split(" ")[0];
+        
+        String email = nameMail + surnameMail + '.' + department + '@' + office + ".goldeneye.com";
+        
+        return ResponseEntity.ok(email);
     }
 }
 
