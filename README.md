9b5194c54c4d205bf6f7046ebc7ace9b74c017d5
AiramLD <airamluisdiaz629@gmail.com>
2024-03-18 10:53:10 +0000

    primeros pasos

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+<<<<<<< HEAD
 # Golden-eye
+=======
+# GoldenEye Project
+
+## What you need
+
+-   JDK 1.8+
+-   Maven 3+
+
+## What it's included into the project
+
+- Spring Boot
+- FreeMarker
+
+## Run de application
+
+    mvn spring-boot:run
+
+>>>>>>> bb5c2b3 (Base del proyecto)
---
+++ src/main/java/com/codebay/goldeneye/WebController.java
@@ -0,0 +1,14 @@
+package com.codebay.goldeneye;
+
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WebController {  
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
